bcdfc3035eb9da5388d14623512957305790a0df
Eduardo Guzman-Diaz <tloz_tlotr@hotmail.com>
2018-11-24 16:57:40 -0500

    comrpessor class

---
+++ src/Compressor/Compressor.java
@@ -0,0 +1,15 @@
+package Compressor;
+
+public class Compressor {
+
+    public String CompressString(String stringToCompress){
+        char[] charArray = stringToCompress.toCharArray();
+        while(charArray.length!=0){
+
+
+        }
+
+        return null;
+    }
+
+}
